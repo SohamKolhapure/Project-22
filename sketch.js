c1f018ee7069435a700c1037cc618737c9924ce0
SohamKolhapure <73540571+SohamKolhapure@users.noreply.github.com>
2020-12-02 09:33:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -56,10 +56,10 @@ function draw() {
 }
 
 function keyPressed() {
- if (keyDown ( DOWN_ARROW)) {
+ if (keyCode === DOWN_ARROW) {
 	// Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
 	
-	Matter.Body = setStatic(packageBody,false);    
+	Matter.Body. setStatic(packageBody,false);    
   }
 }
 
